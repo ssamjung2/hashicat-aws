3004115c900213c1ab98ad2418c71598c5ee7401
ssamjung <ssamjung@gmail.com>
2020-04-09 03:27:17 +0000

    Added support for random joke

--- files/deploy_app.sh
+++ files/deploy_app.sh
@@ -2,6 +2,8 @@
 # Script to deploy a very simple web application.
 # The web app has a customizable image and some text.
 
+curl -H "Accept: text/plain" https://icanhazdadjoke.com/ > /var/www/html/joke
+
 cat << EOM > /var/www/html/index.html
 <html>
   <head><title>Woof!</title></head>
@@ -13,6 +15,13 @@ cat << EOM > /var/www/html/index.html
   <center><h2>Doggies are the best world!  Cats can be nice, but they aren't dogs!</h2></center>
   <p>Welcome to ${PREFIX}'s app. Woot! I'm doing Terraform training for deploying and managing infrastructure as code. "You can do it better :)", said Gopi.
   Yes, I can; Yes, I most certainly can.</p>
+  <p>Here's a random joke:
+EOM
+
+cat var/www/html/joke >> /var/www/html/index.html
+
+cat << EOM >> /var/www/html/index.html
+  </p>
   <!-- END -->
 
   </div>
