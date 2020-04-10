a91cdcd2fbbcc8f9294698e09aabe5496353ca8e
ssamjung <ssamjung@gmail.com>
2020-04-10 03:57:30 +0000

    Added 2nd vpc

--- vpc.tf
+++ vpc.tf
@@ -1,8 +1,19 @@
-resource "aws_vpc" "main" {
-  cidr_block       = "10.100.0.0/16"
-  instance_tenancy = "dedicated"
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "my-vpc"
+  cidr = "10.100.0.0/16"
+
+  azs             = ["us-west-1a", "us-west-1b", "us-west-1c"]
+  private_subnets = ["10.100.1.0/24", "10.100.2.0/24", "10.100.3.0/24"]
+  public_subnets  = ["10.100.101.0/24", "10.100.102.0/24", "10.100.103.0/24"]
+
+  enable_nat_gateway = true
+  enable_vpn_gateway = true
 
   tags = {
-    Name = "${var.prefix}-main-vpc"
+    Terraform = "true"
+    Environment = "dev"
+    name = "${var.prefix}-module-vpc"
   }
 }
