99e1e1f0f02b4887b337a7c2e88ed33374988410
ssamjung <ssamjung@gmail.com>
2020-04-10 04:06:57 +0000

    Added 2nd vpc

--- vpc.tf
+++ vpc.tf
@@ -4,7 +4,7 @@ module "vpc" {
   name = "my-vpc"
   cidr = "10.100.0.0/16"
 
-  azs             = ["us-west-1a", "us-west-1b", "us-west-1c"]
+  azs             = ["us-east-1a", "us-east-1b", "us-east-1c"]
   private_subnets = ["10.100.1.0/24", "10.100.2.0/24", "10.100.3.0/24"]
   public_subnets  = ["10.100.101.0/24", "10.100.102.0/24", "10.100.103.0/24"]
 
