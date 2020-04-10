b27096cd5380995a92a2a41d11d0986118553079
ssamjung <ssamjung@gmail.com>
2020-04-10 02:56:17 +0000

    Added web ingress

--- main.tf
+++ main.tf
@@ -27,6 +27,20 @@ resource aws_security_group "hashicat" {
   vpc_id = aws_vpc.hashicat.id
 
   ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
